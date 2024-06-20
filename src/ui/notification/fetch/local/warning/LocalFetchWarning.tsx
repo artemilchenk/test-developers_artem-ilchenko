34358647ca3c25b9c8a1bf9ba805b41e0e75ad8b
Artem <artemilchenko999@gmail.com>
2024-06-20 18:21:17 +0300

    test

--- src/ui/notification/fetch/local/warning/LocalFetchWarning.tsx
+++ src/ui/notification/fetch/local/warning/LocalFetchWarning.tsx
@@ -1,6 +1,7 @@
 import classes from './warning.module.scss'
 
 export const LocalFetchWarning = ({children})=>{
+    console.log('test')
     return <div className={classes.warning}>
         {children}
     </div>
